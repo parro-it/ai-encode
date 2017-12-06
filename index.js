7587d36c7687ebb5b8646114ca635adc09a4a269
Andrea Parodi <andrea@parro.it>
2017-12-06 23:05:55 +0100

    first version

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-export default async function aiEncode(data) {
-  for (const item of data) {
-    await item;
-  }
+import AsyncIterable from "asynciterable";
+import { TextEncoder } from "util";
+
+/**
+ * Encode a String async iterable into a Buffer async iterable.
+ * @param  {AsyncIterable} iterable The source iterable to encode. It must emit String instances
+ * @return {AsyncIterable}          An async iterable that iterates over the encoded Buffer s
+ */
+export default function aiEncode(iterable) {
+  const encoder = new TextEncoder();
+  const generator = iterable[Symbol.asyncIterator] || iterable[Symbol.iterator];
+  const iterator = generator.call(iterable);
+
+  return new AsyncIterable(async (write, end) => {
+    let item = {};
+    while (!item.done) {
+      item = await iterator.next();
+      if (!item.done) {
+        write(encoder.encode(item.value));
+      }
+    }
+    end();
+  });
 }
--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 import test from "tape-async";
 import aiEncode from ".";
+import { fs } from "ai-node";
+import concat from "ai-concat";
+
+const fixture = `${__dirname}/fixtures/test`;
 
 test("exports a function", async t => {
-  t.is(typeof aiEncode, "function");
+  t.is(typeof fs.readFile, "function");
+});
+
+test("decode an iterable", async t => {
+  const buffer = await concat.buff(aiEncode(fs.readFile(fixture, "utf8")));
+  t.is(buffer.toString("utf8"), "test line 1\nline 2\n");
 });
